ef653616f5cdd35c627e95f36f724f7a878990f9
morzw <morzw@post.bgu.ac.il>
2020-06-12 10:33:51 +0300

    maven, log4j2, example

---
+++ src/main/java/View/Controller.java
@@ -0,0 +1,10 @@
+package View;
+
+import javafx.event.ActionEvent;
+
+public class Controller {
+    public void pressButton(ActionEvent actionEvent) {
+        System.out.println("Hi");
+    }
+
+}
