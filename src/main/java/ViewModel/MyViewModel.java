cb1075b4fb0dcb50539de0e35a3d796fc62ade7a
morzw <morzw@post.bgu.ac.il>
2020-06-12 13:24:00 +0300

    singelton in MyModel and MyViewModel + mazeDisplayer from the tirgul

--- src/main/java/ViewModel/MyViewModel.java
+++ src/main/java/ViewModel/MyViewModel.java
@@ -1,5 +1,6 @@
 package ViewModel;
 
+import Model.IModel;
 import Model.MyModel;
 
 import java.util.Observable;
@@ -8,16 +9,17 @@ import java.util.Observer;
 public class MyViewModel extends Observable implements Observer {
 
     private static MyViewModel myViewModel;
+    private IModel model;
 
     //constructor
-    private MyViewModel() {
-
+    private MyViewModel(IModel model) {
+        this.model = model;
     }
 
     //get instance
-    public static MyViewModel getInstance() {
+    public static MyViewModel getInstance(IModel model) {
         if (myViewModel == null) {
-            myViewModel = new MyViewModel();
+            myViewModel = new MyViewModel(model);
         }
         return myViewModel;
     }
