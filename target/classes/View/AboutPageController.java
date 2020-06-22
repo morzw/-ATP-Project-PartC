83d9e2fea71d10e83871308da86d0df0304b9955
Gal <galagasi5@gmail.com>
2020-06-22 19:34:31 +0300

    Jar

---
+++ target/classes/View/AboutPageController.java
@@ -0,0 +1,27 @@
+package View;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Observable;
+
+public class AboutPageController extends Controller {
+    @FXML
+    public Button algoButton;
+
+    public void aboutAlgo() {
+        changeScene("AlgoPage.fxml",(Stage)algoButton.getScene().getWindow(),"What happens behind the scenes...");
+    }
+
+    public void aboutUs() {
+        changeScene("AboutUsPage.fxml",(Stage)algoButton.getScene().getWindow(),"About us");
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {}
+}
